bda8d423bb54729a5019e3abf6a3abfa98e1558a
Krystian Sosnowski <kr.sosnowski@gmail.com>
2019-10-08 20:45:40 +0200

    Dodano Scanner

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner input = new Scanner(System.in);
+        System.out.println("Podaj liczbę całkowitą:");
+        int k = input.nextInt();
+        for (int i=0; i<=k; i++) {
+            long l = (long)Math.pow(2, i);
+            System.out.println("2 do potęgi " + i + " wynosi " + l);
+        }
     }
 }
